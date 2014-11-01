a43430cdd38a762f5ad03ac624c1ca682c0d1559
Pradeep Chhetri <pradeep@indix.com>
2014-11-02 01:06:50 +0530

    Added some more endpoints

--- src/clj_useragent/handler.clj
+++ src/clj_useragent/handler.clj
@@ -11,10 +11,16 @@
        (generate-string {:user-agent (vec (extract-uas *all-uas*))}))
   (GET "/version/:user-agent" [user-agent]
        (generate-string {:version (vec (extract-versions user-agent))}))
+  (GET "/version/:user-agent/latest" [user-agent]
+       (generate-string {:version (first (extract-versions user-agent))}))
+  (GET "/user-agent-string/random" []
+       (generate-string {:user-agent-string (rand-nth (extract-uas-strings (rand-nth (extract-uas *all-uas*))))}))
   (GET "/user-agent-string/:user-agent" [user-agent]
        (generate-string {:user-agent-string (vec (extract-uas-strings user-agent))}))
   (GET "/user-agent-string/:user-agent/random" [user-agent]
        (generate-string {:user-agent-string (rand-nth (extract-uas-strings user-agent))}))
+  (GET "/user-agent-string/:user-agent/latest" [user-agent]
+       (generate-string {:user-agent-string (first (extract-uas-strings user-agent))}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
